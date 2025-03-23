bd152f85d816a7887849cf8d538c032901190acc
PI-Prasaad-Krishna <prasaad2005@gmail.com>
2025-03-23 07:53:49 +0530

    update 5

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
